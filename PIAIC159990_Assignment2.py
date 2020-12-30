e59fe2492960bf4ea4253b4b253ad5e8e6bcb1dc
khankashan <67883210+khankashan@users.noreply.github.com>
2020-12-30 09:12:10 +0000

    Update PIAIC159990_Assignment2.py

--- PIAIC159990_Assignment2.py
+++ PIAIC159990_Assignment2.py
@@ -180,9 +180,9 @@ def function11():
     # Replace all odd numbers in arr with -1 without changing arr.
     
     arr = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
-    arr[arr%2 == 1] = -1#write your code here 
+    ans = np.where(arr%2!=0,-1,arr)#write your code here 
   
-    return arr
+    return ans
 
     """
     Expected Output:
